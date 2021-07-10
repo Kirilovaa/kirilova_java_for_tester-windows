9ed001e31da9676dadbfada96fdc2fc7ce364178
Kirilovaa <leksa-wyra@mail.ru>
2021-07-10 11:51:08 +0300

    Добавлен класс Collections

---
+++ sandbox/src/main/java/ru/stqa/rft/sandbox/Collections.java
@@ -0,0 +1,11 @@
+package ru.stqa.rft.sandbox;
+
+public class Collections {
+    public static void main (String[] args){
+        String[] langs = {"Java","C#", "Python","PHP"};
+
+        for (int i =0; i < langs.length; i++) {
+            System.out.println("Я хочу выучить " + langs[i]);
+        }
+    }
+}
