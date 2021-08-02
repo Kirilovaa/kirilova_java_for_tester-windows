151a04d36f6c8e6e4e73a83c7749b584659e1e9f
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-08-02 23:12:13 +0300

    Исправлен метод иквелс, в него добавлен id. Исправлена проверка в тесте Создания контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
 
@@ -22,8 +22,8 @@ public class ContactCreationTests extends TestBase {
 //    assertThat(app.contact().count(), equalTo(app.contact().count() + 1));
     Contacts after = app.contact().all();
 
-    assertEquals(after.size(), equalTo(before.size() + 1));
-//    assertThat(app.contact().count(), equalTo(app.contact().count() + 1));
+   // assertEquals(after.size(), equalTo(before.size() + 1));
+    Assert.assertEquals(after.size(),before.size()+1);
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
