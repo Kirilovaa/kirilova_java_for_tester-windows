50040c5f42bb2664957106a32d2f8b8faecafc68
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-08-29 09:59:36 +0300

    Реализованы сравнения списков UI и db для Удаления и Создания Групп, для Создания, Удаления, Модификации контактов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -38,6 +38,7 @@ public class ContactModificationTests extends TestBase {
         assertEquals(app.contact().count(), before.size());
         Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
+        verifyContactListUI();
     }
 
 }
