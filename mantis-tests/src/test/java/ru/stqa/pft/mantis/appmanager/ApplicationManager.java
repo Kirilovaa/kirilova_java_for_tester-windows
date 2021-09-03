d8a0e110da3651acb6576db40ce04d13c2f9f7d2
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-09-03 23:51:19 +0300

    8.3. Прямое взаимодействие с сервером по протоколу HTTP (падает из-за разницы версий приложения)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -43,4 +43,11 @@ public class ApplicationManager {
         wd.findElement(By.linkText("Logout")).click();
         wd.quit();
     }
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
