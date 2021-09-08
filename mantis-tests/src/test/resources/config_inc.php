fb7621cecabfb75130a3dbbe1cd09c122236e9e3
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-09-08 12:28:19 +0300

    Лекция 8.7. Получение почты на внешний почтовый сервер. Создание пользователей на внешнем почтовом сервере (по протоколу Telnet). Добавлен MailHelper,HelperBase,дополнен AppMen,RerHelp,RegTest,config,gradle

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -7,3 +7,5 @@ $g_db_password            = '';
 $g_default_timezone       = 'Europe/Berlin';
 $g_crypto_master_salt     = 'mcpeJsmP3+5vpCK1SwWH55lV9yVYgJgjcDLccznFd90=';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost';
