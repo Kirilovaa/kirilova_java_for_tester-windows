939e20613e4286fc61fb979ed14aa0e438fa27d8
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-09-10 17:55:20 +0300

    9.2. SOAP: простой пример. Добавлен проект soap.

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+import java.nio.charset.StandardCharsets;
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+//    @Test
+//    public void testMyIp() {
+//        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.242.11.185");
+//        assertEquals(ipLocation.get));
+//    }
+}
