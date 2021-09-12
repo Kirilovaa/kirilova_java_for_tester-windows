bd0a079b620604bb6863aab46b1d523162a2fb89
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2021-09-12 22:57:26 +0300

    FIX test: 9.2. SOAP: простой пример

--- soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -1,15 +1,18 @@
 package ru.stqa.pft.soap;
 
 import com.lavasoft.GeoIPService;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.nio.charset.StandardCharsets;
-import static org.testng.Assert.assertEquals;
 
 public class GeoIpServiceTests {
 
-//    @Test
-//    public void testMyIp() {
-//        String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("46.242.11.185");
-//        assertEquals(ipLocation.get));
-//    }
+    @Test
+    public void testMyIp() {
+        String geoIP = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("46.242.11.185");
+        String geoIPcountry = null;
+        if (geoIP.contains("RU")) {
+            geoIPcountry = "RU";
+        }
+        Assert.assertEquals(geoIPcountry, "RU");
+    }
 }
